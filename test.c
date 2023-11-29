57594c685a99f1623f5b72296a430bb8c218148a
Meghdad Dehghan <meghdad@Meghdads-MacBook-Pro.local>
2023-11-29 13:42:33 -0800

    synced with cuda impl

--- test.c
+++ test.c
@@ -8,10 +8,10 @@
 #include <time.h>
 #include <omp.h>
 
-#define VP_W 4.0f
+#define VP_W 0.7f
 #define VP_H VP_W * 9 / 16
 #define DIAFRAGM 0.002f
-#define FOCAL 6.0f
+#define FOCAL 10
 #define R_COUNT 50
 
 void pixel_ray(float x, float y, Vec3 *origin, Vec3 *direction) {
@@ -91,12 +91,6 @@ void trace_ray(Vec3 *origin, Vec3 *direction, int ray_count,
   ray_energy->x /= ray_count;
   ray_energy->y /= ray_count;
   ray_energy->z /= ray_count;
-  // float max_e = fmax(ray_energy->x, fmax(ray_energy->y, ray_energy->z));
-  // if (max_e > 1) {
-  //   ray_energy->x /= max_e;
-  //   ray_energy->y /= max_e;
-  //   ray_energy->z /= max_e;
-  // }
 }
 
 void test_renderer(Scene *scene, Frame *frame, PipelineSetting setting) {
@@ -114,10 +108,9 @@ void test_renderer(Scene *scene, Frame *frame, PipelineSetting setting) {
   UCHAR *b_out = (UCHAR *) malloc(sizeof(UCHAR) * w * h);
   
   // trace rays
-  // #pragma omp parallel for num_threads(16)
+  #pragma omp parallel for num_threads(16)
   for (int x_p = 0; x_p < w * rays; x_p++)
   {
-    #pragma omp parallel for num_threads(16)
     for (int y_p = 0; y_p < h; y_p++)
     {
       int x_p_temp = x_p / rays;
@@ -125,8 +118,8 @@ void test_renderer(Scene *scene, Frame *frame, PipelineSetting setting) {
       int index = y_p * w + x_p;
       unsigned int seed = index + 10;
 
-      float x = ((x_p_temp - w / 2.0) / w) * VP_W * 2,
-             y = -((y_p - h / 2.0) / h) * VP_H * 2;
+      float x = ((x_p_temp - w / 2.0) / w) * VP_W * FOCAL * 2,
+             y = -((y_p - h / 2.0) / h) * VP_H * FOCAL * 2;
       
       Vec3 r_origin;
       Vec3 r_dir;
